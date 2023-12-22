87e638cdcab99e2445a634b6fb372d89c8b2e718
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-22 14:26:17 +0700

    setup: Add Book, Order and Order Detail models

---
+++ MyShop.Core/Models/Book.cs
@@ -0,0 +1,61 @@
+﻿namespace MyShop.Core.Models;
+
+public class Book
+{
+    public int Id
+    {
+        get; set;
+    }
+
+    public string Name
+    {
+        get; set;
+    }
+
+
+    public string Image
+    {
+        get; set;
+    }
+
+    public decimal PurchasePrice
+    {
+        get; set;
+    }
+
+    public decimal SellingPrice
+    {
+        get; set;
+    }
+
+    public string Author
+    {
+
+        get; set;
+    }
+
+    public int PublishedYear
+    {
+        get; set;
+    }
+
+    public decimal RatingsAverage
+    {
+        get; set;
+    }
+
+    public int Quantity
+    {
+        get; set;
+    }
+
+    public string Description
+    {
+        get; set;
+    }
+
+    public int CategoryId
+    {
+        get; set;
+    }
+}
