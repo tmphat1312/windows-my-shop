577944aab3f3ebe4eb8f5244966fca630894359e
Trương Minh Phát <tmphat1312@gmail.com>
2023-12-23 21:19:03 +0700

    app: Add custom controls for first test

---
+++ MyShop/Controls/PaginationControl.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace MyShop.Controls;
+
+public sealed partial class PaginationControl : UserControl
+{
+    public PaginationControl()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ MyShop/Controls/SearchBoxControl.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace MyShop.Controls;
+
+public sealed partial class SearchBoxControl : UserControl
+{
+    public SearchBoxControl()
+    {
+        this.InitializeComponent();
+    }
+}
